424b509a14e7bdbec95f677561a5ef9c93af0ea9
Juan Herrero Macías <jn.herrerom@gmail.com>
2013-11-16 10:44:32 +0100

    detector_Cabecera module added

---
+++ demodulador/detector_Cabecera.vhd
@@ -0,0 +1,66 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    08:40:27 11/16/2013 
+-- Design Name: 
+-- Module Name:    detector_Cabecera - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL; 
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity detector_Cabecera is
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           modulada : in  STD_LOGIC;
+           cabecera_Detectada : out  STD_LOGIC);
+end detector_Cabecera;
+
+-- Esta arquitectura hara un set en la senal cabecera_Detectada siempre que
+-- que las ultimas 5 lecturas de la senal modulada coincidan con la cabecera definida
+
+architecture Behavioral of detector_Cabecera is
+constant cabecera : UNSIGNED (4 downto 0) := "01110"; -- Escribir aquí la cabecera de los bytes de datos
+begin
+	process (clk, reset)
+	variable bit_array : UNSIGNED (4 downto 0);
+	begin
+		if reset = '0' then
+			cabecera_Detectada <= '0';
+		elsif clk'event and clk = '1' then
+			bit_array := shift_left (bit_array, 1);
+			if modulada = '1' then
+				bit_array := (bit_array or "00001");
+			else
+				bit_array := (bit_array and "11110");
+			end if;
+			if bit_array = "01110" then
+				cabecera_Detectada <= '1';
+			else 
+				cabecera_Detectada <= '0';
+			end if;
+		end if;
+	end process;
+end Behavioral;
+
---
+++ demodulador/tb_detector_Cabecera.vhd
@@ -0,0 +1,102 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   10:25:53 11/16/2013
+-- Design Name:   
+-- Module Name:   C:/Users/Juan/Documents/GitHub/modulador/demodulador/tb_detector_Cabecera.vhd
+-- Project Name:  demodulador
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: detector_Cabecera
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+USE ieee.numeric_std.ALL;
+ 
+ENTITY tb_detector_Cabecera IS
+END tb_detector_Cabecera;
+ 
+ARCHITECTURE behavior OF tb_detector_Cabecera IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT detector_Cabecera
+    PORT(
+         clk : IN  std_logic;
+         reset : IN  std_logic;
+         modulada : IN  std_logic;
+         cabecera_Detectada : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal reset : std_logic := '0';
+   signal modulada : std_logic := '0';
+
+ 	--Outputs
+   signal cabecera_Detectada : std_logic;
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: detector_Cabecera PORT MAP (
+          clk => clk,
+          reset => reset,
+          modulada => modulada,
+          cabecera_Detectada => cabecera_Detectada
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+	variable bit_stream : UNSIGNED (9 downto 0) := "0110101110";
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+		reset <= '1';
+      wait for clk_period*10;
+
+      -- insert stimulus here
+		for i in 0 to 100 loop
+			modulada <= bit_stream(0);
+			bit_stream := rotate_left (bit_stream, 1);
+			wait for clk_period;
+		end loop;
+
+      wait;
+   end process;
+
+END;
