a411289b673f0030ca446d2fc1d2c3b7695cfd59
Juan Herrero Macías <jn.herrerom@gmail.com>
2013-11-18 13:09:51 +0100

    detector_Byte module added WIP

---
+++ demodulador/detector_Byte.vhd
@@ -0,0 +1,112 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    10:51:35 11/16/2013 
+-- Design Name: 
+-- Module Name:    detector_Byte - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity detector_Byte is
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+			  modulada : in  STD_LOGIC;
+           cabecera_Detectada : in  STD_LOGIC;
+           leds : out  STD_LOGIC_VECTOR (7 downto 0)
+           );
+end detector_Byte;
+
+architecture Behavioral of detector_Byte is
+
+begin
+process (clk, reset)
+	variable modulada_nand : STD_LOGIC; -- Modulada NAND Portadora
+	variable ones_ctr_nand : INTEGER range 31 downto 0;
+	variable zeros_ctr_nand : INTEGER range 31 downto 0;
+	variable bit_ctr : INTEGER range 8 downto 0; -- Lleva la cuenta de bits leidos
+	variable reading_Flag : STD_LOGIC;
+	variable bit_array : UNSIGNED (4 downto 0);
+	-- Variable para simular la reconstruccion de la portadora
+	variable portadora : STD_LOGIC := '0'; 
+	variable count : INTEGER range 0 to 4;
+	-- n_lecturas_bit -> Numero de lecturas que podemos hacer por bit modulado
+	-- Dependera de la frecuencia de la senal modulada y la de lectura
+	constant n_lecturas_bit : INTEGER := 16; 													  
+	begin
+	-- 
+	-- Lectura de modulada
+		if reset = '0' then
+			leds <= (Others => '0');
+			ones_ctr_nand := 0;
+			zeros_ctr_nand := 0;
+			bit_ctr := 0;
+			reading_Flag := '0';
+			portadora := '0';
+			count := 0;
+		elsif clk'event and clk = '1' then
+			if cabecera_Detectada = '1' then
+				portadora := '1'; -- Sincronizamos portadora
+				count := 1;
+				reading_Flag := '1';
+			end if;
+			if reading_Flag = '1' then
+				modulada_nand := modulada NAND portadora;
+				if modulada_nand = '1' then
+					ones_ctr_nand := ones_ctr_nand + 1;
+				else
+					zeros_ctr_nand := zeros_ctr_nand + 1;
+				end if;
+				if ((ones_ctr_nand + zeros_ctr_nand) = n_lecturas_bit) then
+					-- Procesamos que tipo de bit hemos leido TODO
+					-- Bit 0 ASK 0% NAND - 50% NOR
+					if ((6 < zeros_ctr_nand) and (zeros_ctr_nand < 10)) then
+						leds(bit_ctr) <= '1';
+					else 
+						leds(bit_ctr) <= '0';
+					end if;
+					ones_ctr_nand := 0;
+					zeros_ctr_nand := 0;
+					bit_ctr := bit_ctr + 1;
+					if bit_ctr = 8 then
+						bit_ctr := 0;
+						reading_Flag := '0';
+					end if;
+				end if;
+			end if;	
+		end if;
+	-- Reconstruccion de portadora
+		if reset = '0' then
+			count := 0;
+			portadora := '0';
+		elsif clk'event and clk = '1' then
+			count := count + 1;
+			if count = 2 then
+				portadora := NOT portadora;
+				count := 0;
+			end if;
+		end if;
+	end process;
+end Behavioral;
+
---
+++ demodulador/tb_detector_Byte.vhd
@@ -0,0 +1,153 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   11:02:29 11/18/2013
+-- Design Name:   
+-- Module Name:   C:/Users/Juan/Documents/GitHub/modulador/demodulador/tb_detector_Byte.vhd
+-- Project Name:  demodulador
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: detector_Byte
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+USE ieee.numeric_std.ALL;
+ 
+ENTITY tb_detector_Byte IS
+END tb_detector_Byte;
+ 
+ARCHITECTURE behavior OF tb_detector_Byte IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT detector_Byte
+    PORT(
+         clk : IN  std_logic;
+         reset : IN  std_logic;
+         modulada : IN  std_logic;
+         cabecera_Detectada : IN  std_logic;
+         leds : OUT  std_logic_vector(7 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal reset : std_logic := '0';
+   signal modulada : std_logic := '0';
+   signal cabecera_Detectada : std_logic := '0';
+
+ 	--Outputs
+   signal leds : std_logic_vector(7 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+	
+	
+	-- Procedimiento enviar bit
+	procedure send_bit(
+		bit :  in UNSIGNED (3 downto 0);
+		signal modulada : inout std_logic) is
+		variable bit_copy : UNSIGNED (3 downto 0) := bit;
+	begin
+		for i in 0 to 7 loop
+				modulada <= bit_copy(3);
+				bit_copy := rotate_left (bit_copy, 1);
+				wait for clk_period*2;
+		end loop;
+	end procedure;
+
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: detector_Byte PORT MAP (
+          clk => clk,
+          reset => reset,
+          modulada => modulada,
+          cabecera_Detectada => cabecera_Detectada,
+          leds => leds
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+--	constant bit_ASK_0 : UNSIGNED (15 downto 0) := "0000" & "0000" & "0000" & "0000";
+--	constant bit_FSK_0 : UNSIGNED (15 downto 0) := "0000" & "1111" & "0000" & "1111";
+--	constant bit_PSK_0 : UNSIGNED (15 downto 0) := "1100" & "1100" & "1100" & "1100";
+--	constant bit_1		 : UNSIGNED (15 downto 0) := "0011" & "0011" & "0011" & "0011";
+	constant bit_ASK_0 : UNSIGNED (3 downto 0) := "0000";
+	constant bit_FSK_0 : UNSIGNED (3 downto 0) := "0011";
+	constant bit_PSK_0 : UNSIGNED (3 downto 0) := "1010";
+	constant bit_1		 : UNSIGNED (3 downto 0) := "0101";
+--	variable bit_stream : UNSIGNED (9 downto 0) := "0110101110";
+   begin		
+      -- hold reset state for 100 ns.
+      wait for clk_period*10;
+		reset <= '1';
+      wait for clk_period*10;
+
+      -- insert stimulus here 
+		cabecera_Detectada <= '1';
+		wait for clk_period;
+		cabecera_Detectada <= '0';
+		
+		-- '10110100' byte sent
+		send_bit(bit_PSK_0, modulada);
+		send_bit(bit_ASK_0, modulada);
+		send_bit(bit_1, modulada);
+		send_bit(bit_FSK_0, modulada);
+		send_bit(bit_1, modulada);
+		send_bit(bit_1, modulada);
+		send_bit(bit_FSK_0, modulada);
+		send_bit(bit_1, modulada);
+		
+		wait for clk_period*10;
+		
+		-- New '10100101' byte sent
+		cabecera_Detectada <= '1';
+		wait for clk_period;
+		cabecera_Detectada <= '0';
+		
+		send_bit(bit_1, modulada);
+		send_bit(bit_FSK_0, modulada);
+		send_bit(bit_1, modulada);
+		send_bit(bit_PSK_0, modulada);
+		send_bit(bit_ASK_0, modulada);
+		send_bit(bit_1, modulada);
+		send_bit(bit_FSK_0, modulada);
+		send_bit(bit_1, modulada);
+
+      wait;
+   end process;
+	
+
+END;
